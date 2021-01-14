756b987484c2d56588d262f8855a4a9dfb0e34b6
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-14 10:55:33 -0800

    Styling the Color Slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
-import './Palette.css'
-import Slider, { Range } from 'rc-slider';
+
+import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './Palette.css'
 
 class Palette extends Component {
     constructor(props) {
@@ -21,13 +22,29 @@ class Palette extends Component {
         ))
         return (
             <div className="Palette">
-                <Slider
-                    defaultValue={level}
-                    min={100}
-                    max={900}
-                    step={100}
-                    onAfterChange={this.changeLevel}
-                />
+                <div className="slider">
+                    <Slider
+                        defaultValue={level}
+                        min={100}
+                        max={900}
+                        step={100}
+                        onAfterChange={this.changeLevel}
+                        trackStyle={{ backgroundColor: 'transparent' }}
+                        railStyle={{ height: '8px' }}
+                        handleStyle={{
+                            backgroundColor: 'green',
+                            borderColor: 'green',
+                            height: '13px',
+                            width: '13px',
+                            marginTop: '-3px',
+                        }}
+                        activeDotStyle={{
+                            borderColor: 'green',
+                            boxShadow: 'none',
+                            outline: 'none'
+                        }}
+                    />
+                </div>
                 {/* NAVBAR */}
                 <div className={"Palette-colors"}>
                     {colorBoxes}
