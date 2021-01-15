ce92c4412230cc4c9092b16701a2c5641ca1f12b
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-15 13:10:20 -0800

    Displaying Shades in Single Color Palette

--- src/App.js
+++ src/App.js
@@ -25,10 +25,22 @@ class App extends Component {
           render={routeProps => (
             <Palette
               palette={generatePalette(
-                this.findPalette(routeProps.match.params.id))} />
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
           )}
         />
-        <Route path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} />
+        <Route
+          path="/palette/:paletteId/:colorId"
+          render={routeProps => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
+        />
       </Switch>
 
       // < div className="App" >
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -19,7 +19,7 @@ class ColorBox extends Component {
     }
 
     render() {
-        const { name, background, moreUrl } = this.props
+        const { name, background, moreUrl, showLink } = this.props
         const { copied } = this.state
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -40,9 +40,12 @@ class ColorBox extends Component {
                         <button className="copy-button"> Copy </button>
 
                     </div>
-                    <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-                        <span className='see-more'>More</span>
-                    </Link>
+                    {showLink && (
+                        <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+                            <span className='see-more'>More</span>
+                        </Link>
+                    )}
+
 
                 </div >
             </CopyToClipboard >
