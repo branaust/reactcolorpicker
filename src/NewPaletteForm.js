1f737b7fb899eb85fa2a47a46956514d9507f746
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-22 14:26:44 -0800

    Styling Palette Form Nav

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -20,24 +20,7 @@ const styles = theme => ({
     root: {
         display: "flex"
     },
-    appBar: {
-        transition: theme.transitions.create(["margin", "width"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        })
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(["margin", "width"], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    menuButton: {
-        marginLeft: 12,
-        marginRight: 20
-    },
+
     hide: {
         display: "none"
     },
@@ -162,7 +145,6 @@ class NewPaletteForm extends Component {
             <div className={classes.root}>
                 <PaletteFormNav
                     open={open}
-                    classes={classes}
                     handleSubmit={this.handleSubmit}
                     handleDrawerOpen={this.handleDrawerOpen}
                     palettes={palettes}
