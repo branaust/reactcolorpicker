3c80c872d07abc3d37760dd04ae8040850466c7c
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-22 14:07:36 -0800

    Extract Color Picker Component

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -28,6 +28,9 @@ class ColorPickerForm extends Component {
             name: this.state.newColorName
         }
         this.props.addNewColor(newColor)
+        this.setState({
+            newColorName: ""
+        })
     }
 
 
