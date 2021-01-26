52034b947fcc261e74b74226466e0764e39e8f80
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-25 18:18:06 -0800

    Make Color Box Responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -18,11 +18,11 @@ export default {
         },
         [sizes.down("lg")]: {
             width: "25%",
-            height: props => props.showingFullPalette ? "20%" : "50%",
+            height: props => props.showingFullPalette ? "20%" : "33.333%",
         },
         [sizes.down("md")]: {
             width: "50%",
-            height: props => props.showingFullPalette ? "10%" : "50%",
+            height: props => props.showingFullPalette ? "10%" : "20%",
         },
         [sizes.down("xs")]: {
             width: "100%",
@@ -129,6 +129,9 @@ export default {
             width: "100%",
             marginBottom: "0",
             padding: "1rem",
+            [sizes.down("xs")]: {
+                fontSize: "2.5rem"
+            }
         },
 
         "& p": {
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+import sizes from './sizes'
+
 export default {
     Palette: {
         height: "100vh",
@@ -15,7 +17,7 @@ export default {
         margin: "0 auto",
         display: "inline-block",
         position: "relative",
-        marginBottom: "-3.5px",
+        marginBottom: "-6.2px",
         textTransform: "uppercase",
         opacity: "1",
         backgroundColor: "black",
@@ -37,6 +39,19 @@ export default {
             border: "none",
             cursor: "pointer",
             textDecoration: "none",
-        }
+        },
+        [sizes.down("lg")]: {
+            width: "25%",
+            height: "33.333%"
+        },
+        [sizes.down("md")]: {
+            width: "50%",
+            height: "20%"
+        },
+        [sizes.down("xs")]: {
+            width: "100%",
+            height: "10%"
+        },
+
     }
 }
