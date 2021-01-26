d184e85bbb45787ac6c8b85542d0ee504acbd2eb
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-26 15:17:55 -0800

    Fade Animations w Transition Group

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import MiniPalette from './MiniPalette'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { withStyles } from '@material-ui/styles'
 import styles from './styles/PaletteListStyles'
 
@@ -19,16 +20,19 @@ class PaletteList extends Component {
                         <h1>React Colors</h1>
                         <Link className="" to="/palette/new">Create Palette</Link>
                     </nav>
-                    <div className={classes.palettes}>
+
+                    <TransitionGroup className={classes.palettes}>
                         {palettes.map(palette => (
-                            <MiniPalette {...palette}
-                                handleClick={() => this.goToPalette(palette.id)}
-                                deletePalette={deletePalette}
-                                key={palette.id}
-                                id={palette.id}
-                            />
+                            <CSSTransition key={palette.id} classNames='fade' timeout={500}>
+                                <MiniPalette {...palette}
+                                    handleClick={() => this.goToPalette(palette.id)}
+                                    deletePalette={deletePalette}
+                                    key={palette.id}
+                                    id={palette.id}
+                                />
+                            </CSSTransition>
                         ))}
-                    </div>
+                    </TransitionGroup>
                 </div>
             </div>
 
